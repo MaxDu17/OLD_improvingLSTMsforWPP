7e2b1d7dd2c56c350ffd8a76e04bca7fb76d9bbf
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-06-26 15:54:29 -0400

    got sql to work

--- data_feeder.py
+++ data_feeder.py
@@ -6,16 +6,19 @@ class DataParser:
     data = pd.read_csv("2012DATA_HEADLESS.csv")
     clean_data = data[["Month", "Day", "Hour", "Minute", "power (MW)"]]
     power_ds = data[["power (MW)"]]
-    power_ds.set_index("power (MW)")
+    power_ds.set_index("power (MW)",inplace= True)
 
     def print_from_start(self, number):
         return self.power_ds.head(number)
-    def grab_list(self):
+    def grab_list(self,start, end):
         engine = create_engine('sqlite://', echo=False)
         self.power_ds.to_sql(name = "power", con = engine)
-        return engine
+        command = "SELECT * FROM power LIMIT " + str(start) + "," + str(end)
+        a = engine.execute(command).fetchall()
+        clean = [k[0] for k in a]
+        return clean
 
 dp = DataParser()
-engine = dp.grab_list()
-a= engine.execute("SELECT * FROM power LIMIT 0,1000").fetchall()
-print(a[1])
+answer = dp.grab_list(0,1000)
+
+print(answer[4])
