9ad3510c078a4ad638cc2dcbcea8c7cce7c61816
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-06-26 13:20:06 -0400

    asdfghjkl;'

---
+++ LSTM_test.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+#making the basic structure. I will use an included for now.
+
+def hyper:
+    
+x = tf.placeholder
--- text file generator.py
+++ text file generator.py
@@ -1,3 +1,9 @@
 import csv
 
-file = open 
+file = open("test_file.csv","w")
+writer_log = csv.writer(file, lineterminator="\n")
+for i in range(100):
+    carrier = [i]
+    writer_log.writerow(carrier)
+
+print("done!")
