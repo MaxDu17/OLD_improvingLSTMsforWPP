605ea0c77beca75d0c4f115aa062345c0fc7153e
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-07-03 13:12:35 -0400

    Never gonna make you cry

--- run_frozen_LSTM_v2.py
+++ run_frozen_LSTM_v2.py
@@ -6,7 +6,7 @@ import csv
 
 hyp = Hyperparameters()
 sm = SetMaker()
-pbfilename = "v3/models/LSTM_v3_frozen.pb"
+pbfilename = "v3_1/models/LSTM_v3_1_frozen.pb"
 
 
 with tf.gfile.GFile(pbfilename, "rb") as f:
@@ -27,7 +27,7 @@ with tf.Graph().as_default() as graph:
 
 with tf.Session(graph=graph) as sess:
     sm.create_training_set()
-    test = open("v3/GRAPHS/EVALUATE_TEST.csv", "w")
+    test = open("v3_1/GRAPHS/EVALUATE_TEST.csv", "w")
     test_logger = csv.writer(test, lineterminator="\n")
     carrier = ["true_values", "predicted_values", "abs_error"]
     test_logger.writerow(carrier)
--- save_v2.py
+++ save_v2.py
@@ -1,17 +1,17 @@
 import tensorflow as tf
 from tensorflow.python.tools import freeze_graph
-MODEL_NAME = 'LSTM_v3_frozen'
+MODEL_NAME = 'LSTM_v3_1_frozen'
 
 # Freeze the graph
 
-input_graph_path = "v3/GRAPHS/graph.pbtxt"
-checkpoint_path = "v3/models/LSTMv3-40000"
+input_graph_path = "v3_1/GRAPHS/graph.pbtxt"
+checkpoint_path = "v3_1/models/LSTMv3-60000"
 input_saver_def_path = ""
 input_binary = False
 output_node_names = "output_gate/output"
 restore_op_name = "save/restore_all"
 filename_tensor_name = "save/Const:0"
-output_frozen_graph_name = 'v3/models/'+MODEL_NAME+'.pb'
+output_frozen_graph_name = 'v3_1/models/'+MODEL_NAME+'.pb'
 
 clear_devices = True
 
