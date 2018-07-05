407102fcdb5ecc899b3872190f2b167b7e4ccb9f
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-07-05 17:00:46 -0400

    Your heart's been aching but you're too shy to say it

---
+++ weather_data and parser/get_all_data_labels.py
@@ -0,0 +1,14 @@
+import pygrib
+import csv
+
+writer = open("data.csv", "w")
+recorder = csv.writer(writer, lineterminator="\n")
+test_file = "ruc2anl_130_20110101_0000_000.grb2"
+opened_file = pygrib.open(test_file)
+selection = opened_file.read()
+selection = list(selection)
+print(selection)
+'''
+for single in selection:
+    recorder.writerow(single)
+'''
