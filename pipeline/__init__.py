bc6782a314df7105ae717206f3642c8871518697
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-07-11 11:34:56 -0400

    trying to get multilayer library to work

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -2,3 +2,4 @@ from dataset_maker import SetMaker
 from data_feeder import DataParser
 from hyperparameters import Hyperparameters
 from adaptive_loss_function import My_Loss
+from modelv7 import Model
