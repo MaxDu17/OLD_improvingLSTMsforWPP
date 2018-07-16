6662e8d1e3f630649b4d57223c062fd7ce42dc73
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-07-16 16:24:45 -0400

    accidently tripped run program again

--- data_feeder_v2.py
+++ data_feeder_v2.py
@@ -11,28 +11,29 @@ class DataParser_Weather:
         #self.data = pd.read_csv("Training_sets/83863-2011.csv", skiprows = 3) #read file
         self.data = pd.read_csv("Training_sets/2012DATA.csv", skiprows=3)  # read file
         #clean_data = data[["Month", "Day", "Hour", "Minute", "power (MW)"]] #extract critical data, not used here
-        self.data = self.data[["power (MW)"],["wind direction at 100m (deg)"],["wind speed at 100m (m/s)"],["air temperature at 2m "],["surface air pressure (Pa)"],["density at hub height (kg/m^3)"]] #extract a single column
-
+        self.combined_data = self.data[["power (MW)"],["wind direction at 100m (deg)"],["wind speed at 100m (m/s)"],
+                              ["air temperature at 2m "],["surface air pressure (Pa)"],["density at hub height (kg/m^3)"]] #extracts ALL data
+    '''
     def use_foreign(self, file_name):
         self.data = pd.read_csv(file_name, skiprows = 3) #read file
         #clean_data = data[["Month", "Day", "Hour", "Minute", "power (MW)"]] #extract critical data, not used here
-        self.power_ds = self.data[["power (MW)"]] #extract a single column
-
+        self.combined_data = self.data[["power (MW)"]] #extract a single column
+    '''
     def print_from_start(self, number):
-        return self.power_ds.head(number) #print everything. Seldom used, but is an option
+        return self.combined_data.head(number) #print everything. Seldom used, but is an option
 
     def dataset_size(self):
-        return self.power_ds.size
+        return self.combined_data.size
 
     def grab_list_range(self,start,end): #selects a range to query
-        self.power_ds.index.name = "index" #sets index to "index" for ease of query
+        self.combined_data.index.name = "index" #sets index to "index" for ease of query
         command = str(start) + "<=index<" + str(end) #makes command
-        subset = self.power_ds.query(command) #querys the pandas data frame
+        subset = self.combined_data.query(command) #querys the pandas data frame
         clean = [round(k[0],3) for k in subset.values] #extracts the value and discards the index value
         return clean #returns the query in a form of a list
 
     def grab_element(self, element):
-        array = self.power_ds.values
+        array = self.combined_data.values
         clean_array = [k[0] for k in array]
         return clean_array[element]
 
--- feederandmakertesterv2.py
+++ feederandmakertesterv2.py
@@ -1,2 +1,6 @@
 from pipeline import DataParser_Weather
 from pipeline import SetMaker_Weather
+
+DP = DataParser_Weather()
+
+DP.print_from_start()
