46aa6c3b4c385042876d8ca8cdeea43cc44dfcd0
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-07-16 13:44:33 -0400

    why are my tensors not being found?

--- run_frozen_LSTM_v6.py
+++ run_frozen_LSTM_v6.py
@@ -6,7 +6,7 @@ import csv
 
 hyp = Hyperparameters()
 sm = SetMaker()
-pbfilename = "2012/v2/models_CONTAINED/LSTM_v2_frozen_CONTAINED.pb"
+pbfilename = "2012/v8/models/LSTM_v8_frozen.pb"
 
 
 with tf.gfile.GFile(pbfilename, "rb") as f:
@@ -18,17 +18,19 @@ with tf.Graph().as_default() as graph:
                         input_map = None,
                         return_elements = None,
                         name = "")
+    #Y = graph.get_tensor_by_name("placeholders/label:0")
     inputs = graph.get_tensor_by_name("placeholders/input_data:0")
     init_state_1 = graph.get_tensor_by_name("placeholders/initial_states_1:0")
     init_state_2 = graph.get_tensor_by_name("placeholders/initial_states_2:0")
     output = graph.get_tensor_by_name("layer_2_propagation/output:0")
     pass_back_state_2 = graph.get_tensor_by_name("layer_2_propagation/pass_back_state_2:0")
     pass_back_state_1 = graph.get_tensor_by_name("layer_1_propagation/pass_back_state_1:0")
-    loss = graph.get_tensor_by_name("loss/loss:0")
+    #loss = graph.get_tensor_by_name("loss/loss:0")
 
 with tf.Session(graph=graph) as sess:
+    init_state_1_ = init_state_2_ = np.zeros(shape=[2, 1, hyp.cell_dim])
     sm.create_training_set()
-    test = open("2012/v2/GRAPHS_CONTAINED/EVALUATE_TEST.csv", "w")
+    test = open("2012/v8/GRAPHS/EVALUATE_TEST.csv", "w")
     test_logger = csv.writer(test, lineterminator="\n")
     carrier = ["true_values", "predicted_values", "abs_error"]
     test_logger.writerow(carrier)
@@ -41,11 +43,11 @@ with tf.Session(graph=graph) as sess:
         label = np.reshape(label_, [1, 1])
         data = np.reshape(data, [hyp.FOOTPRINT, 1, 1])
 
-        init_state_1_, init_state_2_, output_, loss_ = sess.run([pass_back_state_1, pass_back_state_2, output, loss], # why passback? Because we only shift by one!
-                                                       feed_dict={Y: label, init_state_1: init_state_1_,
+        init_state_1_, init_state_2_, output_ = sess.run([pass_back_state_1, pass_back_state_2, output], # why passback? Because we only shift by one!
+                                                       feed_dict={init_state_1: init_state_1_,
                                                                   init_state_2: init_state_2_, inputs: data})
-        RMS_loss += np.sqrt(loss_)
-        carrier = [label_, output_[0][0], np.sqrt(loss_)]
+        RMS_loss += np.abs(label_-output_[0][0])
+        carrier = [label_, output_[0][0], np.abs(label_-output_[0][0])]
         test_logger.writerow(carrier)
     RMS_loss = RMS_loss / hyp.Info.TEST_SIZE
     print("test: rms loss is ", RMS_loss)
