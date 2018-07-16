61386534c8fd41b765e8c251f1742ff21b51a647
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-07-16 10:53:14 -0400

    asdfghjkl;'

--- weather_data and parser/all-in-one/all_in_one.py
+++ weather_data and parser/all-in-one/all_in_one.py
@@ -45,7 +45,7 @@ for filename in big_tar_list:
     # Untar each file to its own folder, after it is done, delete the tar file
     dirname = overarching_name.replace('.tar','')
     tarcommand = 'tar -xf '+overarching_name + ' -C ' + dirname + '; rm '+overarching_name + \
-                 '; cd /home/max/SHARED; python3 local_grabber_single.py ' + dirname + " " + filename
+                 '; python3 local_grabber_single.py ' + dirname + " " + filename
     print("Extracting tar: " + filename)
     subprocess.call(["mkdir", "-p", dirname])
     subprocess.Popen(['/bin/sh', '-c', tarcommand])
