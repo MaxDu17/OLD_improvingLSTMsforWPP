630836340780ffe2f42afea7123de02ce2b8b875
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-07-16 13:53:32 -0400

    trying to train again

--- checkfreeze.py
+++ checkfreeze.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 g = tf.GraphDef()
-g.ParseFromString(open("2012/v2/models_CONTAINED/LSTM_v2_frozen_CONTAINED.pb", "rb").read())
-k = [n for n in g.node if n.name.find("loss") != -1] # same for output or any other node you want to make sure is ok
+g.ParseFromString(open("2012/v8/models/LSTM_v8_frozen.pb", "rb").read())
+k = [n for n in g.node if n.name.find("output") != -1] # for output or any other node you want to make sure is ok
 p = g.node
 print(k)
