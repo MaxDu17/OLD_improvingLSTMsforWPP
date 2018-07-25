9fca66f2c2252c40fa8d48b87305efc2bc84f3fa
MaximilianDu <30485928+MaximilianDu@users.noreply.github.com>
2018-07-25 10:01:30 -0400

    lets see if it works now

--- weather_data and parser/local_saver/downloader.py
+++ weather_data and parser/local_saver/downloader.py
@@ -39,7 +39,7 @@ for filename in big_tar_list:
     # Untar each file to its own folder, after it is done, delete the tar file
     dirname = overarching_name.replace('.tar','')
     tarcommand = 'tar -xf '+overarching_name + ' -C ' + dirname + '; rm '+overarching_name + \
-        '; touch ' + path + filename
+        '; cd ' + path + 'touch ' + filename
     print("Extracting tar: " + filename)
     subprocess.call(["mkdir", "-p", dirname])
     subprocess.Popen(['/bin/sh', '-c', tarcommand])
