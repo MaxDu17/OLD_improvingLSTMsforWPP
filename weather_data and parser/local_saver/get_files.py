44ee1f552bffa4166a6af6251a89ecb602434727
Max Du <maxdubusiness@gmail.com>
2018-08-02 13:47:38 -0400

    so confused rn

--- weather_data and parser/local_saver/get_files.py
+++ weather_data and parser/local_saver/get_files.py
@@ -17,7 +17,8 @@ try:
     answer = input("Would you like to delete crash files? (y/n)")
     if answer == 'y':
         print("deleting crash directory")
-        subprocess.call(['rm','*','/home/wedu/database/data/crash/'])
+        subprocess.call(['rm','-r','/home/wedu/database/data/crash/'])
+        subprocess.call(["mkdir", '/home/wedu/database/data/crash/'])
 except:
     print("error, no files found. Quitting...")
     quit()
